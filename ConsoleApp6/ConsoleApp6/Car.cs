c39d934e3da91d5e78b4dafc5de29390bc563ff8
S-goss <70651880+S-goss@users.noreply.github.com>
2020-11-06 15:54:04 -0500

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Car
+    {
+        private int speed = 0;
+
+
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        //C# Class Methods 
+
+            /// <summary>
+            /// Starts the car driving at 10 MPH
+            /// </summary>
+        public void Drive()
+        {
+            speed = 10;
+        }
+        /// <summary>
+        /// Drive methods takes 1 input and updates the variable speed.
+        /// </summary>
+        /// <param name="speed">Speed to set</param>
+
+        public void Drive(int speed)
+        {
+            this.speed = speed;
+        }
+    }
+}
